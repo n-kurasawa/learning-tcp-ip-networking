4b8d6c7b6ef9157f599508d44515e21c36bc442f
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-10-30 11:45:11 +0900

    add build.sh

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+function ns() {
+  sudo ip netns add "${param[1]}"
+}
+
+function delnsall() {
+  sudo ip -all netns delete
+}
+
+function veth() {
+  sudo ip link add "${param[1]}" type veth peer name "${param[2]}"
+}
+
+function setveth() {
+  sudo ip link set "${param[1]}" netns "${param[2]}"
+}
+
+function up() {
+  sudo ip netns exec "${param[1]}" ip link set "${param[2]}" up
+}
+
+function ip() {
+  sudo ip netns exec "${param[1]}" ip address add "${param[2]}" dev "${param[3]}"
+}
+
+function mac() {
+  sudo ip netns exec "${param[1]}" ip link set dev "${param[2]}" address "${param[3]}"
+}
+
+for OPT in "$@"; do
+  param+=("$OPT")
+done
+
+case "${param[0]}" in
+'ns')
+    ns
+    ;;
+'delnsall')
+    delnsall
+    ;;
+'veth')
+    veth
+    ;;
+'setveth')
+    setveth
+    ;;
+'up')
+    up
+    ;;
+'ip')
+    ip
+    ;;
+'mac')
+    mac
+    ;;
+*)
+    echo "[ERROR] $PROGNAME: illegal subcommand -- '$(echo ${param[0]})'"
+    usage
+    exit 1
+    ;;
+esac
